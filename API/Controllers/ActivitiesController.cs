9f8735c8834658c2ae65454057b23f212a6b441f
Momo Johnson <momojohnson@Momos-MacBook-Pro.local>
2024-03-06 15:41:49 -0800

    initial commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseController
+    {
+        private readonly DataContext _datacontext;
+        public ActivitiesController(DataContext dataContext)
+        {
+            _datacontext = dataContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return Ok(await _datacontext.Activities.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity?>> GetActivity(Guid id)
+        {
+            return await _datacontext.Activities.FirstOrDefaultAsync(activity => activity.ID == id);
+        }
+    }
+}
+
---
+++ API/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+
+    }
+}
+
